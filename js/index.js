710cdbda008b635bc1a5d430ca5a7439dbf77f70
Manuel Nuño García <86012117+ManunGar@users.noreply.github.com>
2024-01-29 14:03:37 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+window.onscroll = function () {
+    var y = window.scrollY
+    console.log(y);
+    if ( y >= 5) {
+        document.querySelector(".header").classList.add("scroll");
+    }else {
+        document.querySelector(".header").classList.remove("scroll")
+    } 
+}
+
+
+document.querySelector('.menu').addEventListener('click', function () {
+    document.querySelector('.header').classList.toggle('active');
+})
+
+document.querySelector('.content').addEventListener('click', function () {
+    document.querySelector('.header').classList.remove('active');
+})
